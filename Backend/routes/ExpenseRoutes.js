3a66d60ff768562aaf41dddac314bc45e466823d
dorakensan <dorakendsan@gmail.com>
2025-02-28 00:41:02 +0545

    nothing changes

--- Backend/routes/ExpenseRoutes.js
+++ Backend/routes/ExpenseRoutes.js
@@ -1,7 +1,4 @@
 const express = require("express");
 const Expense = require("../Models/Expense");
-<<<<<<< HEAD
-const authMiddleware=require("../")
-=======
+
 const authMiddleware = require("../");
->>>>>>> middleware
