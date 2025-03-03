2b2b2babd4222e7ad8d5e6e058cae3621d749366
dorakensan <dorakendsan@gmail.com>
2025-03-03 23:37:43 +0545

    routes path

--- Backend/index.js
+++ Backend/index.js
@@ -4,10 +4,24 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("Mongo db connection Failed", err));
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/expenses", require("./routes/ExpenseRoutes"));
+app.use("/api/budget", require("./routes/BudgetRoutes"));
+app.use("/api/export", require("./routes/ExportRoutes"));
+app.use("/api/receipt", require("./routes/ReceiptRoutes"));
+app.use("/api/ai", require("./routes/AiRoutes"));
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Backend is running...");
