060c810f299ef99bee1ae6c44af162fe950d90af
dorakensan <dorakendsan@gmail.com>
2025-03-17 01:11:55 +0545

    resuable button component added

--- Frontend/Finance-Tracker/src/components/Buttons.jsx
+++ Frontend/Finance-Tracker/src/components/Buttons.jsx
@@ -1,7 +1,34 @@
+// components/Button.jsx
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Buttons = () => {
-  return <div>Buttons</div>;
+const Buttons = ({
+  to,
+  children,
+  onClick,
+  type = "button",
+  className = "",
+  disabled = false,
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (!disabled) {
+      navigate(to);
+    }
+  };
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`px-4 py-2 rounded-lg font-medium transition-all duration-200 ${className} ${
+        disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-gray-200"
+      }`}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Buttons;
